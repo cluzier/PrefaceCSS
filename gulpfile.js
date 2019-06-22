f670efc30a59e36e1e2e492a34c270574d6254d1
Conner Luzier <connerluzier@Conners-MacBook-Pro.local>
2019-06-22 00:15:36 -0400

    bg-color, font-color bugs fixed and gulp fixes

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,18 @@ gulp.task('scss', function () {
   return gulp.src('./src/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
-    .pipe(banner('/* css-boilerplate <https://github.com/cluzier/css-boilerplate> , Copyright 2019, Conner Luzier */\n', {}))
-    .pipe(rename('css-boilerplate.css'))
+    .pipe(banner('/* PrefaceCSS <https://github.com/cluzier/PrefaceCSS> , Copyright 2019, Conner Luzier */\n', {}))
+    .pipe(rename('preface.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('scss:min', function () {
+  return gulp.src('./src/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(banner('/* PrefaceCSS <https://github.com/cluzier/PrefaceCSS> , Copyright 2019, Conner Luzier */\n', {}))
+    .pipe(rename('preface.min.css'))
     .pipe(gulp.dest('./dist'));
 });
 
