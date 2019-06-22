f6c84a299607eb686b15a15fcaf1f2ab8253dfd9
Conner Luzier <37819611+cluzier@users.noreply.github.com>
2019-06-21 20:44:45 -0400

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const banner = require('gulp-banner');
+const rename = require('gulp-rename');
+
+gulp.task('scss', function () {
+  return gulp.src('./src/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
+    .pipe(banner('/* css-boilerplate <https://github.com/cluzier/css-boilerplate> , Copyright 2019, Conner Luzier */\n', {}))
+    .pipe(rename('css-boilerplate.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('scss:min', function () {
+  return gulp.src('./src/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({ browsers: ['> 1%', 'IE 7'], cascade: false }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(banner('/* css-boilerplate <https://github.com/cluzier/css-boilerplate> , Copyright 2019, Conner Luzier */\n', {}))
+    .pipe(rename('css-boilerplate.min.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/*.scss', ['scss', 'scss:min']);
+});
